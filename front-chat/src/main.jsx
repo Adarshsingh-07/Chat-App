8e4a2362d5ff73bc7a777a62f5a55f3108ecff9b
Adarsh <adarshsingh8340312805@gmail.com>
2025-12-30 15:59:19 +0530

    Initial commit - real time chat app (Spring Boot + React)

---
+++ front-chat/src/main.jsx
@@ -0,0 +1,15 @@
+import { createRoot } from 'react-dom/client';
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter } from "react-router";
+import AppRoutes from "./config/AppRoutes.jsx";
+import { ChatProvider } from "./context/ChatContext.jsx";
+import './index.css';
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Toaster position="top-center" />
+    <ChatProvider>
+      <AppRoutes />
+    </ChatProvider>
+  </BrowserRouter>
+);
